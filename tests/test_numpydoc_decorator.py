b9e8aee2f13dab228baae78eb821a1939b447e67
Alistair Miles <alimanfoo@googlemail.com>
2023-03-06 18:29:04 +0000

    implement examples section

--- tests/test_numpydoc_decorator.py
+++ tests/test_numpydoc_decorator.py
@@ -1481,7 +1481,7 @@ def test_notes():
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
             The value of :math:`\omega` is larger than 5.
-        """,  # noqa,
+        """,  # noqa
     )
     def foo(bar, baz):
         pass
