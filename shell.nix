63be743317b09265af42324bc019fb32da36515a
Vince Reuter <vince.reuter@gmail.com>
2024-03-17 20:03:45 +0100

    add a few dev dependencies, define group for poetry, and add Nix shell

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{
+  pkgs ? import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs/";
+    ref = "refs/tags/23.11";
+  }) {}, 
+  dev ? true,
+}:
+let 
+  py310 = pkgs.python310;
+  poetryExtras = if dev then ["dev"] else [];
+  poetryInstallExtras = (
+    if poetryExtras == [] then ""
+    else pkgs.lib.concatStrings [ " --with=" (pkgs.lib.concatStringsSep "," poetryExtras) ]
+  );
+in
+pkgs.mkShell {
+  name = "numpy-deco-env";
+  buildInputs = with pkgs; [
+    poetry
+    py310
+    python311
+    python312
+  ];
+  shellHook = ''
+    poetry env use "${py310}/bin/python"
+    poetry install -vv --sync${poetryInstallExtras}
+    source "$(poetry env info --path)/bin/activate"
+  '';
+}
